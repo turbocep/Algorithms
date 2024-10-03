469a22a5a5b3f28d16734c990a582f1266c600f3
turbocep <162988238+turbocep@users.noreply.github.com>
2024-10-03 13:16:03 +0200

    add split array comparison.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -7,11 +7,29 @@
 def merge_sort(array)
   return array if array.length <= 1 #Is this a sufficient base case? I might need another.
 
-  middle_index = array.length / 2
+  middle_index = array.length / 2 #for simpler splitting syntax
 
   left = merge_sort(array[0...middle_index])
   right = merge_sort(array[middle_index..-1])
 
+  sorted_array = []
+  until left.empty? && right.empty? do
+    if left.empty?
+      sorted_array.push(right)
+      right.clear
+    elsif right.empty?
+      sorted_array.push(left)
+      left.clear
+    elsif left.first > right.first
+      sorted_array.push(left.first)
+      left.shift
+    else
+      sorted_array.push(right.first)
+      right.shift
+    end
+  end
+
+  return sorted_array
 end
 
 #Must test whether this method actually splits stuff well.
