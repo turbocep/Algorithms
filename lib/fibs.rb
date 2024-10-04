764092bbd32bfe61418bdf374c05c4695599f18c
turbocep <162988238+turbocep@users.noreply.github.com>
2024-10-04 13:13:59 +0200

    add #fibs_rec solution.

--- lib/fibs.rb
+++ lib/fibs.rb
@@ -1,7 +1,7 @@
 #Takes n and returns an array containing all the fibonacci numbers up to n.
 #Example: n = 8 should return [0, 1, 1, 2, 3, 5, 8, 13]
 
-#Non-recursive version:
+#Non-recursive version: Omega(2). 
 def fibs(n)
   fibo = [0, 1]
   if n == 0
@@ -16,17 +16,27 @@ def fibs(n)
   end
 end
 
-def fibo(num)
-  return num if num <= 1
-  fibo(num - 1) + fibo(num - 2)
-end
+# def fibo(num)
+#   puts 'This was printed recursively.'
+#   return num if num <= 1
+#   fibo(num - 1) + fibo(num - 2)
+# end
+
+# def fibs_rec(n)
+#   final = []
+#   n.times do | num |
+#     final.push(fibo(num))
+#   end
+#   final
+# end
+
+# p fibs_rec(8)
 
 def fibs_rec(n)
-  final = []
-  n.times do | num |
-    final.push(fibo(num))
-  end
-  final
+  return [0, 1] if n == 1
+  s = fibs_rec(n - 1)
+  s.push(s[-1] + s[-2])
+  return s
 end
 
 p fibs_rec(8)
