be0e5bc4fa8cbf3e0d112ee8ff5335ed0cae078d
turbocep <162988238+turbocep@users.noreply.github.com>
2024-10-09 12:49:53 +0200

    bugfix node.rb, add #to_s, #append and #prepend

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,9 +1,52 @@
 require_relative('./node.rb')
 
+#Linked List class
 class LinkedList
-  
+  attr_accessor :head
+
+  def initialize(head = nil)
+    self.head = head
+  end
+
+  def append(value)
+    current = head
+    return self.head = Node.new(value) if head == nil
+    if current.next_node == nil
+      current.next_node = Node.new(value)
+    end
+  end
+
+  def prepend(value)
+    previous = head
+    self.head = Node.new(value, previous)
+  end
+
+  def to_s
+    current = head
+    final = ''
+    loop do
+      if current == nil
+        return final << 'nil'
+      else
+        final << "( #{current.value} ) -> "
+      end
+      current = current.next_node
+    end
+  end
+
 end
 
-node = Node.new
+list = LinkedList.new
 
-p node
+list.append(6)
+
+puts list
+
+list.append(5)
+
+puts list
+
+list.prepend(1)
+
+puts list
+p list
--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,9 @@
 class Node
   attr_accessor :value, :next_node
 
-  def initialize(value = nil)
+  def initialize(value = nil, next_node = nil)
     self.value = value
-    self.next_node = nil
+    self.next_node = next_node
   end
 end
 
